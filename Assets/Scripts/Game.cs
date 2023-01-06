e2488d1fc336d3c05d2dfb431cb10899b6b550ee
Mossyegghead01 <luthfihaidar1176@gmail.com>
2023-01-06 15:17:25 +0700

    Draw Card Button
    Ye just that basically

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,10 +7,8 @@ public class Game : MonoBehaviour
     public GameObject CardTemplate;
     public List<GameObject> Hand = new();
     public int drawAmount;
-    public int cardGap;
-    public int cardOffset;
-    public int cardGapHover;
-    public int cardOffsetHover;
+    public float cardGap;
+    public float cardOffset;
     public GameObject hoveredOnCard = null;
 
     // Start is called before the first frame update
@@ -22,26 +20,34 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (hoveredOnCard != null){
-            if ((Hand.Count)%2 == 0){
-                for (int i=0; i<Hand.Count; i++){
-                    Hand[i].transform.position = Vector3.Lerp(Hand[i].transform.position, new Vector3(((Hand.Count)/2)*-cardGapHover + (cardGapHover*i) + cardOffsetHover, -15, 10 - (i*0.01f)), 0.1f);
-                }
+        if (Hand.Count % 2 == 0)
+        {
+            for (int i = 0; i < Hand.Count; i++)
+            {
+                Hand[i].transform.position = new Vector3((Hand.Count / 2 * -cardGap + (cardGap * i) + cardOffset) * 1.0f + (cardGap / 2.0f), -15, 10 - (i * 0.01f));
             }
-        }else{
-            if ((Hand.Count)%2 == 0){
-                for (int i=0; i<Hand.Count; i++){
-                    Hand[i].transform.position = Vector3.Lerp(Hand[i].transform.position, new Vector3(((drawAmount)/2)*-cardGap + (cardGap*i) + cardOffset, -15, 10 - (i*0.01f)), 0.1f);
-                }
+        }
+        else
+        {
+            for (int i = 0; i < Hand.Count; i++)
+            {
+                Hand[i].transform.position = new Vector3((Hand.Count - 1) / 2 * -cardGap + (cardGap * i) + cardOffset, -15, 10 - (i * 0.01f));
             }
-        }*/
+        }
+
     }
 
     public void startGame()
     {
-        for (int i=0; i<drawAmount; i++){
+        drawCard(drawAmount);
+    }
+
+    public void drawCard(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
             int id = Random.Range(1, CardDatabase.cardList.Count - 1);
-            GameObject Temp = Instantiate(CardTemplate, new Vector3(drawAmount/2*-cardGap + (cardGap*i) + cardOffset, -15, 10 - (i*0.01f)), CardTemplate.transform.rotation, CardTemplate.transform.parent);
+            GameObject Temp = Instantiate(CardTemplate, CardTemplate.transform.position, CardTemplate.transform.rotation, CardTemplate.transform.parent);
             Temp.GetComponent<CardDisplay>().id = id;
             Hand.Add(Temp);
         }
