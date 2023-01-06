e150724263363ead871415ecf4732a0344e9f9e8
snailsquid <hexagononyt@gmail.com>
2023-01-06 23:11:18 +0700

    merge

--- Assets/Scripts/RaycastSelect.cs
+++ Assets/Scripts/RaycastSelect.cs
@@ -11,47 +11,65 @@ public class RaycastSelect : MonoBehaviour
     public Material defaultMaterial;
     public int selectOffset;
 
-    private float duration = 3f;
-    private float elapsedTime;
+    public float speed = 0.5f;
+
+    public Animator[] selectAnimator;
+    public Animator[] unselectAnimator;
 
     private Transform _selection;
 
-    void resetCard(float percentageComplete)
+    void resetCard()
     {
-        if(_selection!= null) { 
-            var selectionRenderer = _selection.GetComponent<Renderer>();
+        if(_selection!= null&&_selection.parent!=null) {
+             var selectionRenderer = _selection.GetComponent<Renderer>();
+            unselectAnimator = _selection.parent.GetComponentsInChildren<Animator>();
+            foreach(Animator animator in unselectAnimator)
+            {
+                animator.Play("UnselectCard", 0, 0.0f);
+            }
             selectionRenderer.material = defaultMaterial;
-            _selection.parent.position = Vector3.Lerp(new Vector3(_selection.parent.position.x,-5, _selection.parent.position.z), new Vector3(_selection.parent.position.x, -15, _selection.parent.position.z),duration);
+            Debug.Log(_selection);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        float percentageComplete = elapsedTime / duration;
-        resetCard(percentageComplete);
-
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit))
         {
             var selection = hit.transform;
-            if (selection.CompareTag(selectableTag)) {
                 var selectionRenderer = selection.GetComponent<Renderer>();
                 if (selectionRenderer!= null)
                 {
                     if (selection == _selection)
                     {
-                        selection.parent.position = Vector3.Lerp(new Vector3(selection.parent.position.x, -15, selection.parent.position.z), new Vector3(selection.parent.position.x, -5, selection.parent.position.z), duration);
+
+                    if (selection.CompareTag(selectableTag))
+                    {
                         selectionRenderer.material = highlightMaterial;
                     }
-                    else { 
-                        resetCard(percentageComplete);
+                    } else {
+                    if (_selection != null&&selection.parent!=null&&_selection.GetComponent<Renderer>()!=null)
+                    {
+                        if (selectionRenderer.material != _selection.GetComponent<Renderer>().material)
+                        {
+                            Debug.Log("first highlight");
+                            //selection.parent.position = new Vector3(selection.parent.position.x, -5, selection.parent.position.z);
+                            selectAnimator = selection.parent.GetComponentsInChildren<Animator>();
+                            foreach (Animator animator in selectAnimator)
+                            {
+                                animator.Play("SelectCard", 0, 0.0f);
+                            }
+                        }
+                    }
+                    Debug.Log("no highlight");
+                        resetCard();
                     }
                 } 
                     _selection = selection;
             }
-        }
+        
     }
 }
